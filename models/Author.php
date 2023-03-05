0d613f557036b5e76875cded208694981e3e7620
MinhDuckk <minhducnguyen257@gmail.com>
2023-03-05 21:57:54 +0700

    update: thêm file mvc author

--- models/Author.php
+++ models/Author.php
@@ -1,5 +1,33 @@
-<?php 
-    class Author {
-        
+<?php
+class Author{
+    private $maTgia;
+    private $tenTgia;
+    private $hinhAnh;
+    public function __construct($maTgia,$tenTgia,$hinhAnh){
+        $this->maTgia = $maTgia;
+        $this->tenTgia = $tenTgia;
+        $this->hinhAnh = $hinhAnh;
     }
+
+
+    // Setters
+    public function setTenTgia($tenTgia) {
+        $this->tenTgia = $tenTgia;
+    }
+    public function setHinhAnh($hinhAnh) {
+        $this->hinhAnh = $hinhAnh;
+    }
+
+    // Getters
+    public function getMaTgia() {
+        return $this->maTgia;
+    }
+
+    public function getTenTgia() {
+        return $this->tenTgia;
+    }
+    public function getHinhAnh() {
+        return $this->hinhAnh;
+    }
+}
 ?>
